6d04effae33b66911399a0df36e9eb6f495d38e5
Nandor Licker <licker.nandor@gmail.com>
2015-04-22 00:17:23 +0100

    Boilerplate for renderer.

---
+++ client/js/gfx/mesh.js
@@ -0,0 +1,71 @@
+// This file is part of the Shapy project.
+// Licensing information can be found in the LICENSE file.
+// (C) 2015 The Shapy Team. All rights reserved.
+goog.provide('shapy.gfx.Mesh');
+
+
+
+/**
+ * Mesh class with static factory method for specific meshes.
+ *
+ * @constructor
+ */
+shapy.gfx.Mesh = function() {
+
+};
+
+
+/**
+ * Renders the mesh.
+ */
+shapy.gfx.Mesh.prototype.render = function() {
+
+};
+
+
+/**
+ * Checks if the mesh intersects a ray.
+ */
+shapy.gfx.Mesh.prototype.intersects = function(ray) {
+
+};
+
+
+/**
+ * Creates a plane.
+ */
+shapy.gfx.Mesh.createPlane = function(gl, w, h, sx, sy) {
+
+};
+
+
+/**
+ * Creates an arrow.
+ */
+shapy.gfx.Mesh.createArrow = function(gl, dir) {
+
+};
+
+
+/**
+ * Creates a cube.
+ */
+shapy.gfx.Mesh.createCube = function(gl, w, h, d) {
+
+};
+
+
+/**
+ * Creates a sphere.
+ */
+shapy.gfx.Mesh.createSphere = function(gl, r, phi, theta) {
+
+};
+
+
+/**
+ * Creates a circle.
+ */
+shapy.gfx.Mesh.createCircle = function(gl, norm, r) {
+
+};
