e1102487651ccb864033cb3f24c03e431135d214
Ilija Radosavovic <ilija.radosavovic13@imperial.ac.uk>
2015-05-28 00:54:28 +0100

    Removed window files.

--- client/js/window.js
+++
@@ -1,9 +0,0 @@
-// This file is part of the Shapy project.
-// Licensing information can be found in the LICENSE file.
-// (C) 2015 The Shapy Team. All rights reserved.
-goog.provide('shapy.WindowController');
-
-
-/**
- * Controller for pop up windows.
- */
