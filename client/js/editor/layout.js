2e22bf0ff1aece74ed52fd2d6fd26d9f60d71e87
Nandor Licker <licker.nandor@gmail.com>
2015-06-06 16:02:55 +0100

    Only a single UV viewport allowed.

--- client/js/editor/layout.js
+++ client/js/editor/layout.js
@@ -100,11 +100,17 @@ shapy.editor.Layout.prototype.getViewport_ = function(x, y) {
 shapy.editor.Layout.prototype.toggleUV = function() {
   var name = this.active.name;
 
-  if (this.active.type == shapy.editor.Viewport.Type.UV) {
-    this[name] = this.viewports[name] = new shapy.editor.Viewport.Edit(name);
-  } else {
-    this[name] = this.viewports[name] = new shapy.editor.Viewport.UV(name);
-  }
+  this.viewports = goog.object.map(this.viewports, function(vp, vpName) {
+    if (vpName != name && vp.type != shapy.editor.Viewport.Type.UV) {
+      return vp;
+    }
+    if (vpName != name) {
+      this[vpName] = new shapy.editor.Viewport.Edit(vpName);
+    } else {
+      this[vpName] = new shapy.editor.Viewport.UV(vpName);
+    }
+    return this[vpName];
+  }, this);
 
   this.active = this[name];
   this.active.active = true;
