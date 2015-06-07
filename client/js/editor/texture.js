44242ef2d791471b2552d20c0f0d9e4a48753416
Nandor Licker <licker.nandor@gmail.com>
2015-06-07 21:45:46 +0100

    Smoother brush.

--- client/js/editor/texture.js
+++ client/js/editor/texture.js
@@ -43,11 +43,11 @@ shapy.editor.Texture.prototype.build_ = function(gl) {
   gl.texParameteri(
       goog.webgl.TEXTURE_2D,
       goog.webgl.TEXTURE_MAG_FILTER,
-      goog.webgl.NEAREST);
+      goog.webgl.LINEAR);
   gl.texParameteri(
       goog.webgl.TEXTURE_2D,
       goog.webgl.TEXTURE_MIN_FILTER,
-      goog.webgl.NEAREST);
+      goog.webgl.LINEAR);
 };
 
 
@@ -90,16 +90,33 @@ shapy.editor.Texture.prototype.paint = function(u, v, colour, size) {
   var x = Math.floor(u * this.width);
   var y = Math.floor(v * this.height);
 
-  size = 2;
+  size = 5;
   for (var i = y - size; i <= y + size; ++i) {
     for (var j = x - size; j <= x + size; ++j) {
       if (i < 0 || this.width <= i || j < 0 || this.height <= j) {
         continue;
       }
-      var idx = i * this.width + j;
-      this.data_[idx * 3 + 0] = colour[0];
-      this.data_[idx * 3 + 1] = colour[1];
-      this.data_[idx * 3 + 2] = colour[2];
+
+      var a = 1.0 - Math.sqrt(
+          Math.pow((j - x) / size, 2) + Math.pow((i - y) / size, 2));
+      a = Math.max(Math.min(a, 1.0), 0.0);
+
+      var k = i * this.width + j;
+
+      var r = this.data_[k * 3 + 0];
+      var g = this.data_[k * 3 + 1];
+      var b = this.data_[k * 3 + 2];
+
+      r = r * (1 - a) + colour[0] * a;
+      g = g * (1 - a) + colour[1] * a;
+      b = b * (1 - a) + colour[2] * a;
+      r = Math.max(Math.min(r, 255), 0);
+      g = Math.max(Math.min(g, 255), 0);
+      b = Math.max(Math.min(b, 255), 0);
+
+      this.data_[k * 3 + 0] = r;
+      this.data_[k * 3 + 1] = g;
+      this.data_[k * 3 + 2] = b;
     }
   }
   this.dirty_ = true;
