ecd9ef7b801410ba18f95e341c3661281c793b9a
Michal Sienkiewicz <mich.sienkiewicz@gmail.com>
2015-06-07 15:49:24 +0100

    Proper querying for sharing data

--- shapy/permissions.py
+++ shapy/permissions.py
@@ -21,14 +21,47 @@ class PermissionsHandler(APIHandler):
     # Validate arguments.
     if not user:
       raise HTTPError(401, 'Not authorized.')
+    id = int(self.get_argument('id'))
+
+    # Check if user owns a non-dir asset with given id
+    cursor = yield momoko.Op(self.db.execute,
+      '''SELECT 1
+         FROM assets
+         WHERE type <> %s
+           AND owner = %s
+           AND id = %s
+      ''', (
+      'dir',
+      user.id,
+      id
+    ))
+    data = cursor.fetchone()
+    if not data:
+        raise HTTPError(404, 'Asset not owned by user.')
+
+    # Fetch available emails and permissions currently granted
+    cursors = yield momoko.Op(self.db.transaction, (
+    (
+    '''SELECT email
+       FROM users
+    '''
+    ),
+    (
+    '''SELECT users.email, permissions.write
+       FROM users
+       INNER JOIN permissions
+       ON users.id = permissions.user_id
+       WHERE permissions.asset_id = %s
+    ''',
+      (
+        id,
+      )
+    )
+    ))
 
     # Return JSON answer.
     self.write(json.dumps({
-      'available': ['mich.sienkiewicz@gazeta.pl',
-                    'mich.sienkiewicz@gmail.com',
-                    'ilija.radosavovic@gmail.com',
-                    'trelemorele@gmail.com'],
-      'shared': [{'email': 'mich.sienkiewicz@gazeta.pl', 'write': True} for i in range(7)] +
-                [{'email': 'mich.sienkiewicz@gazeta.pl', 'write': False} for i in range(7)]
+      'available': [item[0] for item in cursors[0].fetchall()],
+      'shared': [{'email': item[0], 'write': item[1]} for item in cursors[1].fetchall()]
     }))
     self.finish()
