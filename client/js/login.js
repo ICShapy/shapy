d3f26a9e98531ee8ec543a574751d4d3cd67fd72
Michal Sienkiewicz <mich.sienkiewicz@gmail.com>
2015-06-08 01:09:29 +0100

    Clean cache on login, fixes blocked assets page bug

--- client/js/login.js
+++ client/js/login.js
@@ -11,16 +11,19 @@ goog.provide('shapy.LoginController');
  * @constructor
  * @ngInject
  *
- * @param {!angular.$state} $state Angular state.
- * @param {!shapy.AuthService} shUser Authentication service.
+ * @param {!angular.$state}        $state    Angular state.
+ * @param {!shapy.AuthService}     shUser    Authentication service.
+ * @param {!shapy.browser.Service} shBrowser The browser service.
  */
-shapy.LoginController = function($state, shUser) {
+shapy.LoginController = function($state, shUser, shBrowser) {
   /** @public {string} */
   this.password = '';
   /** @public {string} */
   this.username = '';
   /** @private {!shapy.AuthService} @const */
   this.shUser_ = shUser;
+  /** @private {!shapy.browser.Service} @const */
+  this.shBrowser_ = shBrowser;
   /** @private {!angular.$state} @const */
   this.state_ = $state;
 };
@@ -31,6 +34,7 @@ shapy.LoginController = function($state, shUser) {
  */
 shapy.LoginController.prototype.login = function() {
   this.shUser_.login(this.username, this.password).then(goog.bind(function() {
+    this.shBrowser_.clearAllCache();
     this.state_.go('main', null, { reload: true });
   }, this));
 };
